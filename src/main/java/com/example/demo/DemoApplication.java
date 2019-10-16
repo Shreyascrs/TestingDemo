159f8a6352eb1634fe528d0a5e05178b0883d558
shreyascr <shrs601@gmail.com>
2019-10-16 23:31:05 +0530

    adding branch

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("branch1");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
